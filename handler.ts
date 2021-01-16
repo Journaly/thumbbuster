455ed31a7f3ccab280081b41eaa8784a945b0d23
Ryan Lanny Jenkins <rjenkins@styleseat.com>
2021-01-16 10:25:00 -0600

    Generate the two sizes of image we need for post headers.

--- handler.ts
+++ handler.ts
@@ -7,21 +7,29 @@ const s3 = new AWS.S3()
 export const handleUpload: S3Handler = async (event, contex) => {
   for (let i=0; i<event.Records.length; i++) {
     const rec = event.Records[i]
-    console.log(rec)
     const inputImage = await s3.getObject({
       Bucket: rec.s3.bucket.name,
       Key: rec.s3.object.key,
     }).promise()
 
-    const resized = await sharp(inputImage.Body).resize(150).toBuffer();
+    const input = sharp(inputImage.Body)
 
-    await s3.putObject({
+    const t = async (size: number, suffix: string) => {
+      const transformed = await input.clone()
+        .resize(size, null, { withoutEnlargement: true })
+        .toBuffer()
+
+      return s3.putObject({
         Bucket: process.env.TRANSFORM_BUCKET!,
-        Key: rec.s3.object.key + '-tiny',
-        Body: resized,
+        Key: `${rec.s3.object.key}-${suffix}`,
+        Body: transformed,
         ContentType: "image"
-    }).promise(); 
-  }
+      }).promise()
+    }
 
-  return { message: `Done!` }
+    await Promise.all([
+      t(1200, 'large'),
+      t(400, 'small'),
+    ])
+  }
 }
